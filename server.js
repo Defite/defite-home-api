eadcadaa7fc676da75151e223297f3c9c0361a4a
Defite <me@defite.ru>
2017-11-18 12:31:21 +0300

    MongoDB config moved to env

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const db = require('./config/db');
 const app = express();
 
 const port = 8000;
@@ -9,7 +8,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(process.env.MONGODB_URL, (err, database) => {
   if(err) {
     return console.log(err);
   }

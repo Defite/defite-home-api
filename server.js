de83d9d6b89476c56b113574a04567b5fbf463cb
Defite <me@defite.ru>
2017-11-18 12:42:24 +0300

    Set port at runtime

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT;
 app.use(bodyParser.urlencoded({
   extended: true
 }));
